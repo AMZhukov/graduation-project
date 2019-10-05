8f76dcbdbbf09cb5195e4ca2c9f126117ae99d80
Andrey Zhukov <zhukov290791@gmail.com>
2019-10-05 19:50:12 +0500

    graduation project, fifth commit

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import viewDropList from './modules/viewDropList';
 
 viewDropList('ul', '.clubs-list');
 modalWindows( '.free-visit', '#gift', '.close-btn', '.form-content'); // Вызов модального окна «Записаться на бесплатные визит»
-//modalWindows( '#callback_form', '#callback_form', 'close-btn', '.form-content');
+modalWindows( '.callback-btn', '#callback_form', 'close-btn', '.form-content'); // Вызов модального окна «Перезвоните мне»
